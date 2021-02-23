5bd02b1b404e888ddbca6fc868f4d37f197d1898
Zeddboy <kunagg25@gmail.com>
2021-02-23 15:14:23 +0530

    update

--- src/main/java/calculator/Calculate.java
+++ src/main/java/calculator/Calculate.java
@@ -1,9 +1,21 @@
 package calculator;
 
 public class Calculate {
-
-	public Calculate() {
-		// TODO Auto-generated constructor stub
+	
+	public static int add(int x, int y)
+	{
+		return x+y;
 	}
-
+	public static int subtract(int x, int y)
+	{
+		return x-y;
+	}
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		System.out.println(add(x,y));
+		System.out.println(subtract(x,y));
+	}
 }
+
+
